990a462f37c22a78819c5a4c14dbd0f63e48d300
tecdevetec <lucashfdantas@gmail.com>
2024-10-27 09:55:07 -0300

    Add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -7,6 +7,16 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+            MainPage = new Login();
+        }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
         }
     }
 }
